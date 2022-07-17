ab39902875d12619a0af6e4bf1cb34ac78529053
awangran <ashleero.yin@gmail.com>
2022-07-17 00:58:39 -0500

    d

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+const firstfield=document.getElementById("type");
+  firstfield.addEventListener("change",(e)=>{
+      if(firstfield.value=="top"){
+          document.getElementById("otherTop").classList.remove("hidden");
+}
+})
